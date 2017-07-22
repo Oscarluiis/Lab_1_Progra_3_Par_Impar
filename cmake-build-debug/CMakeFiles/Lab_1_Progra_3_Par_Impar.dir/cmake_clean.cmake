098aff3f44cc25900e4a791cdca24fffe9626cfa
Oscarluiis <tinocoscar11@unitec.edu>
2017-07-21 19:02:12 -0600

    Ejercicio de números pares e impares C++.
    Hecho con while y do-while.
    Nota: comentado ambos casos para que los puedan analizar.
    
    Ejercicio de Laboratorio.

---
+++ cmake-build-debug/CMakeFiles/Lab_1_Progra_3_Par_Impar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_1_Progra_3_Par_Impar.dir/main.cpp.o"
+  "Lab_1_Progra_3_Par_Impar.pdb"
+  "Lab_1_Progra_3_Par_Impar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_1_Progra_3_Par_Impar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+
+//Ingresar valor a evaluar;
+
+
+    /*  int numero = 0;
+      cout << "Ingrese el valor a evaluar o -1 para salir :";
+      cin >> numero;
+  while(numero != -1){
+
+      if((numero%2) == 0)
+      {
+          cout <<"Es par";
+
+      }else{
+          cout<<"Es impar";
+      }
+      cout<< "\nIngrese el valor a evaluar o -1 para salir :";
+      cin>> numero;
+  }
+      return 0;
+  }
+  */
+   // int numero=0;
+/* Do-While
+    do{
+    cout<<"\nIngrese el numero a evaluar, -1 para salir :";
+    cin>> numero;
+
+    if((numero%2)==0){
+        cout<<"\nEs par";
+    }else
+        cout<<"\nEs impar";
+
+}while(numero != -1);
+
+    return 0;
+}
+
+ */
